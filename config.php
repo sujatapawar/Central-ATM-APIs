ff06dafa0398dbf74ae34577807023db015e7bf0
ShripadKulkarni <shripad.kulkarni@nichelive.com>
2017-09-20 15:02:44 +0530

    Corrected DB credentials

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_NAME', "ATM");
-define('DB_USER', "dbmail");
-define('DB_PASSWORD', "operahouse");
-define('DB_HOST', "localhost");
+define('DB_USER', "juvlonui");
+define('DB_PASSWORD', "#u2dwfbeZlJO");
+define('DB_HOST', "172.16.8.115");
 define('MAIL_HOST', "email-smtp.us-east-1.amazonaws.com");
 define('MAIL_USERNAME', "AKIAIPDG5EDXPIJFECDA");
 define('MAIL_PASSWORD', "ArkhvLuvGelk+vDYoR+UA/z0HWFUvOnMM5//1m+l9WcI");
