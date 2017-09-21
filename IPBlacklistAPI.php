ef239beb6bdbf617dc11a9ba115cf948a2ebddc7
ShripadKulkarni <shripad.kulkarni@nichelive.com>
2017-09-21 14:22:52 +0530

    Added IPBlacklistAPI.php

---
+++ IPBlacklistAPI.php
@@ -0,0 +1,10 @@
+<?php
+/*
+1. Remove IP from Pool (childPool_IPs) and put into freezer (pool 2)
+2. Replace with warm-up IP (pool id 1) with same environment and same grade, with the last stage.
+3. Release all IPs, and update counts
+4. Notify client and client servicing
+5. Notify delivery team
+*/
+
+?>
