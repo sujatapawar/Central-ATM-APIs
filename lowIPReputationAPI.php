8068241a794cd1eabbc1fa7362ea2b7a99982493
ShripadKulkarni <shripad.kulkarni@nichelive.com>
2017-09-21 14:09:51 +0530

    Added lowIPReputationAPI.php

---
+++ lowIPReputationAPI.php
@@ -0,0 +1,15 @@
+<?php
+/*
+1. Remove IP from Pool (childPool_IPs) and put into warm-up (pool id 1)
+2. Replace with warm-up IP (pool id 1) with same environment and same grade, with the last stage.
+3. Release IP, and update counts
+4. Add new entry into client_IP_detail with new IP assignment
+5. Notify delivery team
+6. Return (IP, PMTA, Domain)
+
+*/
+
+
+
+
+?>
