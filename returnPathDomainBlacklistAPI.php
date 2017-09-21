ceda77866dbfeaae62afe9edd6408464ce5323a1
ShripadKulkarni <shripad.kulkarni@nichelive.com>
2017-09-21 14:47:40 +0530

    Added returnPathDomainBlacklistAPI.php

---
+++ returnPathDomainBlacklistAPI.php
@@ -0,0 +1,10 @@
+<?php
+/*
+1. Put Domain into freezer
+2. Replace with a new domain (with same grade and environment, and last stage) from warm-up
+3. Release all IPs, and update counts
+4. Notify client, client servicing and delivery team
+
+*/
+
+?>
