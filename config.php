539abc1b86f5a8b0e827775c23d29442ff488c1b
ShripadKulkarni <shripad.kulkarni@nichelive.com>
2017-09-23 14:43:53 +0530

    Corrected DB credentials

--- config.php
+++ config.php
@@ -1,8 +1,12 @@
 <?php
-define('DB_NAME', "ATM");
-define('DB_USER', "juvlonui");
-define('DB_PASSWORD', "#u2dwfbeZlJO");
-define('DB_HOST', "172.16.8.115");
+define('ATM_DB_NAME', "ATM");
+define('ATM_DB_USER', "juvlonui");
+define('ATM_DB_PASSWORD', "#u2dwfbeZlJO");
+define('ATM_DB_HOST', "172.16.8.115");
+define('DBMAIL_MASTER_DB_NAME', "dbmail_master");
+define('DBMAIL_MASTER_DB_USER', "juvlonui"); 
+define('DBMAIL_MASTER_DB_PASSWORD', "#u2dwfbeZlJO");
+define('DBMAIL_MASTER_DB_HOST', "juvlon-ui-qa-dev.cds73nbgyt7o.us-east-1.rds.amazonaws.com");
 define('MAIL_HOST', "email-smtp.us-east-1.amazonaws.com");
 define('MAIL_USERNAME', "AKIAIPDG5EDXPIJFECDA");
 define('MAIL_PASSWORD', "ArkhvLuvGelk+vDYoR+UA/z0HWFUvOnMM5//1m+l9WcI");
