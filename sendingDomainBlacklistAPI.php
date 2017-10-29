cf558bcacc10dadf9f0f007c175ee7a41d764086
ShripadKulkarni <shripad.kulkarni@nichelive.com>
2017-10-29 12:25:53 +0530

    Update sendingDomainBlacklistAPI.php

--- sendingDomainBlacklistAPI.php
+++ sendingDomainBlacklistAPI.php
@@ -1,21 +1,63 @@
 <?php
 /*
-1. Find all IP_ids for the blacklisted sending domain
-2. Put sending domain into freezer
-3. Switch to the alternate (inactive) domain
-4. If there's no alternate domain found, all IPs should become inactive.
-5. Send email to service, delivery and client.
-{
-"Steps to be done later:
-1. Fetch new sending_domain from warm-up domain
-2. Assign new domain as alternate (inactive) domain for all the IPs.
-3. Replace rDNS config file with alternate domain (requires restart which should happen once a day using a chron job).
-4. Replace PMTA config file with alternate domain 
-(requires restart which should happen once a day using a chron job while the compiler is not sending any emails to the PMTAs). - Change DNS settings."
-
-}
-
+1.  Put the listed domain into freezer.
+2.  Put IPs belongs to the listed domain into new pool called available_assets.
+3.  Replace the same IPs in all child pools with new IP from warmup. 
+5.  Send email to service, delivery and client.
 */
+include("commonFunctions.php");
+///////////////////////////////////PROGRAM INPUT//////////////////////////////////////////////////
+$jsonString = '{"req1":59,"domain":"mail.yesbank.in","ip_wise_counts":{"342":3000,"352":2000}}';
+////////////////////////////////////////////////////////////////////////////////////////////////////
+if(isset($jsonString) and $jsonString!="")
+{
+	//log file a name.
+	$today_date = date("Y-m-d");
+	$csvFileName = 'logs/Sending_Domain_Blacklisted/'.$today_date.'.csv';
+	$logsArray["Date/Time"]=date("Y-m-d H:i:s");
+	$logsArray["Input JSON "]=str_replace(","," ",$jsonString);
+    $obj = new commonFunctions($jsonString);
+     $blacklistedDomainIdArr = $obj->getDomainId($obj->inputJsonArray['domain']);
+   
+    $logsArray["Action1"]="Domain Removed";
+  ////////////////////////////////////////   Coding Needed //////////////////
+  
+  
+  ///////////////////////////////////////////////////////////////////////////
+  
+    //Insert bad domain id into frezzer
+    $obj->putDomainInFreezer($blacklistedDomainId,"childPool_LinkDomains");
+    $logsArray["Action3"]="Domain put into Freezer";
+	
+	//Releasing IP
+	$obj->releaseIP();
+	$logsArray["Action4"]=$json = "IPs are released";
+	    
+	//update IP wise count
+	$obj->UpdateIPWiseCounts();
+	$logsArray["Action5"]="IP wise counts are updated";
+    //write logs
+	if (file_exists($csvFileName)) {
+	$fp = fopen($csvFileName, 'a');
+	} else {
+	$fp = fopen($csvFileName, 'w');
+	 fputcsv($fp, array_keys($logsArray));
+	}
+	//Loop through the associative array.
+	fputcsv($fp, $logsArray);
+	//Finally, close the file pointer.
+	fclose($fp);
+	//Send email alert to client
+	$to="sarah.gidwani@nichelive.com";
+	$subject="[Central ATM API] Email Alert to client for Sending domain Blacklist ";
+	$message="Email Alert for Sending domain Blacklist from Central ATM API";
+	$obj->sendEmailAlert($to,$subject,$message);
+	//Send email alert to delivery team 
+	$to="sarah.gidwani@nichelive.com";
+	$subject="Central ATM API] Email Alert to Deliver for Sending domain Blacklist ";
+	$message="Email Alert for Sending domain Blacklist from Central ATM API";
+	$obj->sendEmailAlert($to,$subject,$message);
+}
 
 
 
