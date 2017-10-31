5e21653fe5780dd841a05ac60f155ca410c083ba
maheshjagdale <mahesh.jagdale@nichelive.com>
2017-10-31 12:24:33 +0530

    Update linkImageDomainBlacklistAPI.php

--- linkImageDomainBlacklistAPI.php
+++ linkImageDomainBlacklistAPI.php
@@ -24,6 +24,7 @@ include("commonFunctions.php");
 //$jsonString = '{"req1":59,"domain":"ml10.juvldc10.net","ip_wise_counts":{"342":3000,"352":2000}}';
 $jsonString = file_get_contents('php://input');
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+$obj = new commonFunctions($jsonString);
 if(isset($jsonString) and $jsonString!="")
 {
 
@@ -34,7 +35,7 @@ if(isset($jsonString) and $jsonString!="")
 	$logsArray["Date/Time"]=date("Y-m-d H:i:s");
 	$logsArray["Input JSON "]=str_replace(","," ",$jsonString);
 
-    $obj = new commonFunctions($jsonString);
+    
 
      $blacklistedDomainIdArr = $obj->getDomainId($obj->inputJsonArray['domain']);
     $blacklistedDomainId=$blacklistedDomainIdArr[0]['domain_id']; //die;
