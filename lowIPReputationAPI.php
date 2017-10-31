fa203c74413f243b15adae056ad89f761caba175
maheshjagdale <mahesh.jagdale@nichelive.com>
2017-10-31 12:25:07 +0530

    Update lowIPReputationAPI.php

--- lowIPReputationAPI.php
+++ lowIPReputationAPI.php
@@ -15,6 +15,7 @@ include("commonFunctions.php");
 //$jsonString = '{"req1":59,"ip_id":342,"ip_wise_counts":{"342":3000,"352":2000}}';
 $jsonString = file_get_contents('php://input');
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+$obj = new commonFunctions($jsonString);
 if(isset($jsonString) and $jsonString!="")
 {
 
@@ -25,7 +26,7 @@ if(isset($jsonString) and $jsonString!="")
 	$logsArray["Date/Time"]=date("Y-m-d H:i:s");
 	$logsArray["Input JSON "]=str_replace(","," ",$jsonString);
 	
-    $obj = new commonFunctions($jsonString);
+    
 
     $lowRepIPId = $obj->inputJsonArray['ip_id'];
 
