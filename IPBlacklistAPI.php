602a439037bd79a670ae23e734c985382322b5d3
ShripadKulkarni <shripad.kulkarni@nichelive.com>
2017-12-01 15:57:03 +0530

    put $

--- IPBlacklistAPI.php
+++ IPBlacklistAPI.php
@@ -31,7 +31,7 @@ if(isset($jsonString) and $jsonString!="")
 
      $blacklistedIPId = $obj->inputJsonArray['ip_id'];
      // update Req1
-     obj->updateReq1Status("Stopped");		
+     $obj->updateReq1Status("Stopped");		
 
     //Retain 'childPool_id' of all pools with given IP_Id in an array 
     $childPoolIdsArray = $obj->getAllChildPoolIds($blacklistedIPId);
