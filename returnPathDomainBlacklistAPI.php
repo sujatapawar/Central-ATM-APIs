4173cccc21cb0c95643776c54990a13bfb9d8302
Mahesh Jagdale <mahesh.jagdale@nichelive.com>
2017-12-15 18:45:42 +0530

    change email notification

--- returnPathDomainBlacklistAPI.php
+++ returnPathDomainBlacklistAPI.php
@@ -22,15 +22,27 @@ if(isset($jsonString) and $jsonString!="")
 
 	$logsArray["Date/Time"]=date("Y-m-d H:i:s");
 	$logsArray["Input JSON "]=str_replace(","," ",$jsonString);
+	$jsonData = json_decode($jsonString,true);
+	$AccountBlockStatus =0;
 
-    
-      $logsArray["Request Type"]=$obj->get_request_type();
+    $IP_IDs = array_keys($jsonData['ip_wise_counts']);
+	$PMTAList = array();
+	$obj->connection_atm();
+	foreach($IP_IDs as $IP_ID)
+	{
+		$Domain = $obj->_dbHandlepdo->sql_Select("domain_master", "domain_id", " where IP_id=?", array($IP_ID));
+		$PMTAName = $obj->_dbHandlepdo->sql_Select("Domain_MTA_mapping", "mta_name", " where domain_id=?", array($Domain[0]['domain_id']));
+		$PMTAList[] = $PMTAName[0]['mta_name'];
+	}
+	$obj->connection_disconnect();
+	
+     /* $logsArray["Request Type"]=$obj->get_request_type();
       // update Req1
      $obj->updateReq1Status("Stopped");		     
 
      $blacklistedDomainIdArr = $obj->getDomainId($obj->inputJsonArray['domain']);
     $blacklistedDomainId=$blacklistedDomainIdArr[0]['domain_id']; //die;
-
+	
     //Retain 'childPool_id' of all pools with given domain id in an array 
     $childPoolIdsArray = $obj->getAllChildPoolIdsOfDomains($blacklistedDomainId,"childPool_RPDomains");
 	//print_r($childPoolIdsArray); //die;
@@ -64,7 +76,14 @@ if(isset($jsonString) and $jsonString!="")
     $logsArray["Action3"]="Domain put into Freezer";
 	
 	//Releasing IP
-	$obj->releaseIP();
+	$IPID = $obj->releaseIP();
+	$IPRelease = array();
+	$obj->connection_atm();
+	foreach($IPID as $I)
+	{
+	$IPRelease = $obj->_dbHandlepdo->sql_Select("IP_master", "IP", " where IP_id=?", array($I['IP_id']));
+	}
+	$obj->connection_disconnect();
 	$logsArray["Action4"]=$json = "IPs are released";
 	    
 	//update IP wise count
@@ -83,7 +102,7 @@ if(isset($jsonString) and $jsonString!="")
     
 	    $obj->connection_db_mail_master();
 		$array = array($Req1_Details[0]['cl_id']);
-		$Client_Details = $obj->_dbHandlepdo->sql_Select("client_master", "cl_name,cl_company", " where cl_id=?", $array);
+		$Client_Details = $obj->_dbHandlepdo->sql_Select("client_master", "cl_name,cl_company,pool_id", " where cl_id=?", $array);
 
 		$Client_Data = "ClientName: ".$Client_Details[0]['cl_name']."\n Company Name:".$Client_Details[0]['cl_company']."\n Mailer-ID:".$Req1_Details[0]['mailer_id']."\n Sent Date:".$Req1_Details[0]['created_time']."\n Total Sent:".$Req1_Details[0]['total_unique_mail'];
 		$array=array(22,$Req1_Details[0]['cl_id'],$Req1_Details[0]['mailer_id'],date('Y-m-d H:i:s'),$Req1_Details[0]['created_time'],$Client_Data,'open');
@@ -98,7 +117,7 @@ if(isset($jsonString) and $jsonString!="")
 		    $obj->_dbHandlepdo->sql_insert("client_blocked_functions", " blocked_function_id,exception_id,client_id", $array);
 		    $array = array(33,$Exception_ID,$Req1_Details[0]['cl_id']);
 		    $obj->_dbHandlepdo->sql_insert("client_blocked_functions", " blocked_function_id,exception_id,client_id", $array);
-		  
+			$AccountBlockStatus =1;
 		}
 	    $obj->connection_disconnect();	
 	
@@ -120,22 +139,58 @@ if(isset($jsonString) and $jsonString!="")
 	//Loop through the associative array.
 	fputcsv($fp, $logsArray);
 	//Finally, close the file pointer.
-	fclose($fp);
+	fclose($fp);*/
 
 
 	//Send email alert to client
-	$to="sarah.gidwani@nichelive.com";
-	$subject="[Central ATM API] Email Alert to client for Returnpath domain Blacklist ";
-	$message="Email Alert for Returnpath domain Blacklist from Central ATM API";
-	$obj->sendEmailAlert($to,$subject,$message);
+	$to = array("mahesh.jagdale@nichelive.com","shripad.kulkarni@nichelive.com");
+	$subject="Your mailing ".$obj->req1." has been discontinued";
+	$message  = "Dear ".$Client_Details[0]['cl_name'].",";
+	$message .= "<p>Your mailing (details below) has caused our return path domain to be blacklisted. In order to protect further degradation of our infrastructure, your mailing has been stopped.</p>";
+	$message .= "<table><tr><td><b>Client: </b></td><td>".$Client_Details[0]['cl_name']." (ID: ".$Req1_Details[0]['cl_id'].")</td></tr>";
+	$message .= "<tr><td><b>Email: </b></td><td>(ID: ".$Req1_Details[0]['mailer_id'].")</td></tr>";
+	$message .= "<tr><td><b>Sending Request ID: </b></td><td>".$obj->req1."</td></tr>";
+	$message .= "<tr><td><b>Return Path Domain: </b></td><td>".$obj->inputJsonArray['domain']."</td></tr>";
+	$message .= "<tr><td><b>Total Recipients: </b></td><td>".$Req1_Details[0]['total_unique_mail']."</td></tr>";
+	$message .= "<tr><td><b>Total Sent:</b></td><td>-</td></tr></table>";
+	$message .= "<p>Please see the log(s) attached that clearly show the blacklisting has occurred during the mailing. This shows that your list has people that may not have subscribed to receive your emails.</p>";
+	$message .= "<p>Your mailing has degraded our infrastructure which will cause delivery problems for other clients using our software. As per Juvlon Terms of Use, credits will not be refunded for emails that were not sent.</p>";
+	$message .= "Sincerely<br/>";
+	$message .= "Juvlon Support";
+	foreach($to as $t)
+	{
+		$obj->sendEmailAlert($t,$subject,$message);
+	}
 
-	//Send email alert to delivery team 
-	$to="sarah.gidwani@nichelive.com";
-	$subject="Central ATM API] Email Alert to Deliver for Returnpath domain Blacklist ";
-	$message="Email Alert for Returnpath domain Blacklist from Central ATM API";
-	$obj->sendEmailAlert($to,$subject,$message);
-
-
+	$to = array("mahesh.jagdale@nichelive.com","shripad.kulkarni@nichelive.com");
+	$subject="Return Path domain ".$obj->inputJsonArray['domain']." blacklisted while sending out ".$obj->req1." for ".$Client_Details[0]['cl_name']." (".$Req1_Details[0]['cl_id'].")";
+	$AccountBlockStatus = ($AccountBlockStatus==1)?"Yes":"No";
+	$message  = "Hi,";
+	$message .= "<p>The Juvlon delivery system has detected a return path domain blacklisting during the sending activity of a client. As a result, the client's sending has been stopped and some changes have been made in certain pools to ensure that the return path domain does not get used for another sending.</p>";
+	$message .= "<p>Please find below the details of the blacklisted return path domain and the sending that caused the blacklisting:</p>";
+	$message .= "<table><tr><td><b>Client: </b></td><td>".$Client_Details[0]['cl_name']." (ID: ".$Req1_Details[0]['cl_id'].")</td></tr>";
+	$message .= "<tr><td><b>Email: </b></td><td>(ID: ".$Req1_Details[0]['mailer_id'].")</td></tr>";
+	$message .= "<tr><td><b>Sending Request ID: </b></td><td>".$obj->req1."</td></tr>";
+	$message .= "<tr><td><b>Client's Pool ID: </b></td><td>".$Client_Details[0]['pool_id']."</td></tr>";
+	$message .= "<tr><td><b>Blacklisted return path domain: : </b></td><td>".$obj->inputJsonArray['domain']."</td></tr>";
+	$message .= "<tr><td><b>Total Recipients: </b></td><td>".$Req1_Details[0]['total_unique_mail']."</td></tr>";
+	$message .= "<tr><td><b>Total Sent:</b></td><td>-</td></tr>";
+	$message .= "<tr><td><b>Environment: </b></td><td>".$Req1_Details[0]['total_unique_mail']."</td></tr>";
+	$message .= "<tr><td><b>List of PMTAs where this job ID was killed: </b></td><td>".implode(',',array_unique($PMTAList))."</td></tr>";
+	$message .= "<tr><td><b>IPs released: </b></td><td>".implode(",",$IPRelease[0])."</td></tr>";
+	$message .= "<tr><td><b>Client's sending functions blocked? </b></td><td>".$AccountBlockStatus."</td></tr></table>";
+	$message .= "<p>Please see the log(s) attached that clearly show the blacklisting has occurred during the mailing.</p>";
+	$message .= "<p>Please find below the changes made to replace the blacklisted return path domain:</p>";
+	$message .= "<p>Blacklisted return path domain moved to: Freezer</p>";
+	$message .= "<p>Pool IDs from where the return path domain was removed: <list of all pool ids where the blacklisted return path domain belonged></p>";
+	$message .= "<p>New return path domain picked from warm-up: ".$obj->inputJsonArray['domain']." (id: ".$blacklistedDomainId.") / None (no appropriate return path domains available in warm-up pool)</p>";
+	$message .= "<p>Pool IDs where the new return path domain is added: <list of all pool ids> / None (if no return path domain was found from the warm-up pool)</p>";
+	$message .= "Sincerely<br/>";
+	$message .= "Juvlon Support";
+	foreach($to as $t)
+	{
+		$obj->sendEmailAlert($t,$subject,$message);
+	}
 }
 else
 {
