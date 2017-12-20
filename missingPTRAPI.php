8e4ae3f59c95d2f83524207e749789f7d40ccfea
ShripadKulkarni <shripad.kulkarni@nichelive.com>
2017-12-20 12:06:35 +0530

    Comment out hardcoded Json i/p

--- missingPTRAPI.php
+++ missingPTRAPI.php
@@ -10,8 +10,8 @@
 include("commonFunctions.php");
 
 ///////////////////////////////////PROGRAM INPUT//////////////////////////////////////////////////
-$jsonString = '{"req1":294,"ip_id":342,"ip_wise_counts":{"342":0,"352":0}}';
-//$jsonString = file_get_contents('php://input');
+//$jsonString = '{"req1":294,"ip_id":342,"ip_wise_counts":{"342":0,"352":0}}';
+$jsonString = file_get_contents('php://input');
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 $obj = new commonFunctions($jsonString);
 if(isset($jsonString) and $jsonString!="")
