487376b3d07bafd51d57a325faac0b392da97fa5
ShripadKulkarni <shripad.kulkarni@nichelive.com>
2018-01-08 12:00:33 +0530

    Changes for stand alone test

--- sendingDomainBlacklistAPI_New.php
+++ sendingDomainBlacklistAPI_New.php
@@ -7,11 +7,12 @@
 */
 include("commonFunctions.php");
 ///////////////////////////////////PROGRAM INPUT//////////////////////////////////////////////////
-//$jsonString = '{"req1":312,"domain":"nl1.sendm.net","ip_wise_counts":{"342":0,"352":0}}';
-$jsonString = file_get_contents('php://input');
+$jsonString = '{"req1":312,"domain":"nl1.sendm.net","ip_wise_counts":{"342":0,"352":0}}';
+//$jsonString = file_get_contents('php://input');
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 $AccountBlockStatus = 0;
 $obj = new commonFunctions($jsonString);
+$obj->updateReq1Status("Stopped",4);	die;
 if(isset($jsonString) and $jsonString!="")
 {
 	//log file a name.
