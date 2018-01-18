b852917f3c790a12bb87f0a31a2500cb4ac42302
maheshjagdale <mahesh.jagdale@nichelive.com>
2018-01-18 16:30:35 +0530

    Update app.js

--- Node-API/app.js
+++ Node-API/app.js
@@ -8,7 +8,11 @@ var con = config.con;
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(3000,function(){ console.log('listen port : 3000');});
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('running at http://' + host + ':' + port)
+});
 
 var AuthID = config.AuthID;
 var AuthPassword = config.AuthPassword;
