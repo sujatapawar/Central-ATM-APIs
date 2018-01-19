fab9191e0f67c46e6037e3c36d432ab0eeb96d34
mahesh <mahesh.jagdale@nichelive.com>
2018-01-19 12:24:34 +0530

    change var and function

--- Node-API/app.js
+++ Node-API/app.js
@@ -1,54 +1,54 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var request = require('request');
-var config  = require('./config.js');
-var func = require('./db_functions');
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const config  = require('./config.js');
+const func = require('./db_functions');
 
-var con = config.con;
-var app = express();
+const con = config.con;
+const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-var server = app.listen(3000,"0.0.0.0" ,function () {
-  var host = server.address().address;
-  var port = server.address().port;
+const server = app.listen(3000,"0.0.0.0" ,() => {
+  const host = server.address().address;
+  const port = server.address().port;
   console.log('running at http://' + host + ':' + port)
 });
 
-var AuthID = config.AuthID;
-var AuthPassword = config.AuthPassword;
-var ZoneDomainName = config.ZoneDomainName;
-var Host = config.Host;
-var Type = config.Type;
-var ServerAPIKey = config.APIKey;
+const AuthID = config.AuthID;
+const AuthPassword = config.AuthPassword;
+const ZoneDomainName = config.ZoneDomainName;
+const Host = config.Host;
+const Type = config.Type;
+const ServerAPIKey = config.APIKey;
 
-app.get('/',function(req,res){
+app.get('/',(req,res)=>{
   res.json({"status":"Error","statusDescription":"Invalid Request."});
 });
 
-app.post('/AddPTR',function(req,res){
-  var keyIP = req.connection.remoteAddress;
-  var ClientAPIKey = req.body.APIKey;
-  var IP = req.body.IP.trim();
-  var Domain = (typeof req.body.Domain == 'undefined')?"":req.body.Domain.trim();
-  var Zone = IP.split(".").reverse();
+app.post('/AddPTR',(req,res)=>{
+  const keyIP = req.connection.remoteAddress;
+  const ClientAPIKey = req.body.APIKey;
+  let IP = req.body.IP.trim();
+  let Domain = (typeof req.body.Domain == 'undefined')?"":req.body.Domain.trim();
+  let Zone = IP.split(".").reverse();
   IP = Zone[0];
-  var NewZone = Zone[1]+'.'+Zone[2]+'.'+Zone[3];
+  let NewZone = Zone[1]+'.'+Zone[2]+'.'+Zone[3];
 
-  var Domain = (typeof req.body.Domain == 'undefined')?"":req.body.Domain.trim();
+  Domain = (typeof req.body.Domain == 'undefined')?"":req.body.Domain.trim();
   //Valid IP
-  func.is_valid_IP(keyIP,ClientAPIKey,ServerAPIKey,con,function(result){
+  func.is_valid_IP(keyIP,ClientAPIKey,ServerAPIKey,con,(result) => {
     if(result)
     {
-      func.is_PTR_Exist(AuthID,AuthPassword,NewZone,ZoneDomainName,Host,Type,function(ID){
+      func.is_PTR_Exist(AuthID,AuthPassword,NewZone,ZoneDomainName,Host,Type,(ID)=>{
         if(ID=='')
         {
-          func.AddPTR(AuthID,AuthPassword,NewZone,ZoneDomainName,Host,Domain,function(result){
+          func.AddPTR(AuthID,AuthPassword,NewZone,ZoneDomainName,Host,Domain,(result)=>{
             res.send(result);
           });
         }
         else
         {
-          func.update_PTR(AuthID,AuthPassword,NewZone,ZoneDomainName,ID,Host,Domain,function(result){
+          func.update_PTR(AuthID,AuthPassword,NewZone,ZoneDomainName,ID,Host,Domain,(result)=>{
             res.send(result);
           });
         }
@@ -61,26 +61,26 @@ app.post('/AddPTR',function(req,res){
   });
 });
 
-app.post('/DeletePTR',function(req,res){
-  var keyIP = req.connection.remoteAddress;
-  var ClientAPIKey = req.body.APIKey;
-  var IP = req.body.IP.trim();
-  var Domain = (typeof req.body.Domain == 'undefined')?"":req.body.Domain.trim();
-  var Zone = IP.split(".").reverse();
+app.post('/DeletePTR',(req,res)=>{
+  const keyIP = req.connection.remoteAddress;
+  const ClientAPIKey = req.body.APIKey;
+  let IP = req.body.IP.trim();
+  let Domain = (typeof req.body.Domain == 'undefined')?"":req.body.Domain.trim();
+  let Zone = IP.split(".").reverse();
   IP = Zone[0];
-  var NewZone = Zone[1]+'.'+Zone[2]+'.'+Zone[3];
+  let NewZone = Zone[1]+'.'+Zone[2]+'.'+Zone[3];
   
-  func.is_valid_IP(keyIP,ClientAPIKey,ServerAPIKey,con,function(result){
+  func.is_valid_IP(keyIP,ClientAPIKey,ServerAPIKey,con,(result)=>{
     if(result)
     {
-      func.is_PTR_Exist(AuthID,AuthPassword,NewZone,ZoneDomainName,Host,Type,function(ID){
+      func.is_PTR_Exist(AuthID,AuthPassword,NewZone,ZoneDomainName,Host,Type,(ID)=>{
         if(ID=='')
         {
           res.json({"status":"Error","statusDescription":"Please enter valid record."});
         }
         else
         {
-          func.DeletePTR(AuthID,AuthPassword,NewZone,ZoneDomainName,ID,function(result){
+          func.DeletePTR(AuthID,AuthPassword,NewZone,ZoneDomainName,ID,(result)=>{
             res.send(result);
           });
         }
