c41482f79921a3003c7e9e46dc5f5ed60d562f41
ShripadKulkarni <shripad.kulkarni@nichelive.com>
2018-01-19 17:06:51 +0530

    Update sendingDomainBlacklistAPI_New.php

--- sendingDomainBlacklistAPI_New.php
+++ sendingDomainBlacklistAPI_New.php
@@ -7,7 +7,7 @@
 */
 include("commonFunctions.php");
 ///////////////////////////////////PROGRAM INPUT//////////////////////////////////////////////////
-$jsonString = '{"req1":395,"domain":"nl1.sendm.net","ip_wise_counts":{"342":1},"otherReq1":{"0":382,"2":273}}';
+$jsonString = '{"req1":395,"domain":"nl1.sendm.net","ip_wise_counts":{"342":1},"otherReq1":[382]}';
 //$jsonString = file_get_contents('php://input');
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 $AccountBlockStatus = 0;
