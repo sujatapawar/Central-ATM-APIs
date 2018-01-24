7862b6326e707b0f8308c6747c334c5aea4366b9
mahesh <mahesh.jagdale@nichelive.com>
2018-01-24 19:12:10 +0530

    test

--- IPBlacklistAPI.php
+++ IPBlacklistAPI.php
@@ -4,7 +4,7 @@
 2. Replace with warm-up IP (pool id 1) with same environment and same grade, with the last stage.
 3. Release all IPs, and update counts
 4. Notify client and client servicing
-5. Notify delivery team
+5. Notify delivery team 
 */
 
 
