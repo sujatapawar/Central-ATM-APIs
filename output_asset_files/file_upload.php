7026bd5a8085066ac6126d2fac272d7cc1256bd7
mahesh <mahesh.jagdale@nichelive.com>
2018-02-07 13:05:22 +0530

    add folder output asset files

---
+++ output_asset_files/file_upload.php
@@ -0,0 +1,23 @@
+<?php
+$files = glob(getcwd()."/*.csv");
+foreach($files as $f):
+    $FileName = basename($f);
+    $TargetURL = "http://localhost/Central-ATM-APIs/output_assetFiles/update_asset.php";
+    if (function_exists('curl_file_create')) 
+    { 
+        $cFile = curl_file_create($f);
+    }
+    else 
+    { 
+        $cFile = '@' . realpath($f);
+    }
+    $post = array('file_contents'=> $cFile);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL,$TargetURL);
+    curl_setopt($ch, CURLOPT_POST,1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+    echo $result=curl_exec($ch);
+    curl_close ($ch);
+endforeach;
+?>
---
+++ output_asset_files/update_asset.php
@@ -0,0 +1,38 @@
+<?php
+include "../commonFunctions.php";
+$obj = new commonFunctions("Demo");
+
+if(!empty($_FILES))
+{
+    $FileName = $_FILES["file_contents"]["name"];
+    if($FileName == "IPAsset.csv")
+    {
+        if(($handle = fopen($_FILES['file_contents']['tmp_name'], 'r')) !== FALSE) 
+        {
+            while(($data = fgetcsv($handle)) !== FALSE) 
+            {
+                $status = explode(",",$data[3]);
+                if(in_array(1,$status))
+                {
+                   echo "Blacklist";
+                }
+                else
+                {
+                    $IP_ID = $data[0];
+                    if($obj->checkIPAssignInDomainMaster($IP_ID))
+                    {
+                        $obj->removeIPFromFreezer($IP_ID);
+                        $obj->putIPInWarmup($IP_ID);
+                    }
+                    else
+                    {
+                        $obj->removeIPFromFreezer($IP_ID);
+                        $obj->putAssetIntoAvailablePool($IP_ID,'IP');
+                    }
+                }
+            }
+        } 
+    }
+    
+}
+?>
